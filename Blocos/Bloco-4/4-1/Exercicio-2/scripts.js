9d5caed90ab8c5b387d40db346d77655d317b38f
Cainan Coutinho <cainan6697@gmail.com>
2020-08-27 18:24:05 -0300

    Ajuste no arquivo script, add ligaçao do arquivo html com o script

---
+++ Blocos/Bloco-4/4-1/Exercicio-2/scripts.js
@@ -0,0 +1,5 @@
+let nome = "Cainan" // String literal
+let idade = 23 // Number literal
+let aprovado = true // Boolean
+let sobreNome = undefined // Undefined
+let corSelecionada = null // Redefinir valor
