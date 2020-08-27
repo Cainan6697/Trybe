2f29e43d0bc6e47348a544639b64e4114e0ebb1e
Cainan Coutinho <cainan6697@gmail.com>
2020-08-27 17:55:46 -0300

    Criaçao de variaveis usando let, feito

---
+++ Blocos/Bloco-4/4-1/Exercicio-1/script.js
@@ -0,0 +1,10 @@
+//Definindo Variáveis:
+
+let nome = "Cainan";
+console.log(nome);
+let idade = 23;
+console.log(idade);
+let altura = 173;
+console.log(altura);
+let nacionalidade = "Brasileira";
+console.log(nacionalidade);
