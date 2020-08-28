709e51f2c4b142c4f1cbe5376b5e2f40cedc3fc1
Cainan Coutinho <cainan6697@gmail.com>
2020-08-28 18:50:31 -0300

    Começando exercicio 4, maior ou menor que 20

---
+++ Blocos/Bloco-4/4-2/Exercicio-4/maiorOuN.js
@@ -0,0 +1,2 @@
+// 4. Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
