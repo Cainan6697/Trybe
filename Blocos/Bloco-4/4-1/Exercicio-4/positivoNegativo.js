8f93bc93687aca0c72d930a62928be613f86d14a
Cainan Coutinho <cainan6697@gmail.com>
2020-08-27 21:11:34 -0300

    Exercicio 4, determinar se o numero eh positivo ou negativo, feito

---
+++ Blocos/Bloco-4/4-1/Exercicio-4/positivoNegativo.js
@@ -0,0 +1,13 @@
+// 4. Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let x = 5;
+let y = -5;
+let z = 0;
+
+if (x > 0) {
+  console.log("positive");
+} else if (x < 0) {
+  console.log("negative")
+} else {
+  console.log("zero")
+}
