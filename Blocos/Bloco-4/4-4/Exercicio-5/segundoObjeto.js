048b8134ec03a8764abf40a5a8c70ac82f4844f4
Cainan Coutinho <cainan6697@gmail.com>
2020-09-01 19:42:53 -0300

    Exercicio-5 finalizado

--- Blocos/Bloco-4/4-4/Exercicio-5/segundoObjeto.js
+++ Blocos/Bloco-4/4-4/Exercicio-5/segundoObjeto.js
@@ -1,2 +1,24 @@
 // 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
 
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "sim",
+};
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "sim",
+}
+
+for (let chave in info && info2) {
+  if (info[chave] === "sim" && info2[chave] === "sim") {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[chave] + " e " + info2[chave]);
+  } 
+}
+
