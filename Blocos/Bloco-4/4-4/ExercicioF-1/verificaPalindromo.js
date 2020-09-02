9612bf8c51b7ecc26e64093165be67d0ce721b2c
Cainan Coutinho <cainan6697@gmail.com>
2020-09-01 21:01:31 -0300

    ExercicioF-1 finalizado

--- Blocos/Bloco-4/4-4/ExercicioF-1/verificaPalindromo.js
+++ Blocos/Bloco-4/4-4/ExercicioF-1/verificaPalindromo.js
@@ -1,2 +1,15 @@
 // 1. Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
 
+function verificaPalindrome(palavra) {
+  let palindromo = "";
+  for (let i = palavra.length - 1; i >= 0; i -= 1) {
+    palindromo += palavra[i];
+  } if (palindromo === palavra) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(verificaPalindrome("arara"));
+console.log(verificaPalindrome("desenvolvimento"));
