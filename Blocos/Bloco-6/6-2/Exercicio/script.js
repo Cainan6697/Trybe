ed16db7da4f40089a6ee35239c0b763f892ffd98
Cainan Coutinho <cainan6697@gmail.com>
2020-10-05 20:35:46 -0300

    Exercicio Form

---
+++ Blocos/Bloco-6/6-2/Exercicio/script.js
@@ -0,0 +1,38 @@
+let estados = [
+  'Acre',
+  'Alagoas',
+  'Amapá',
+  'Amazonas',
+  'Bahia',
+  'Ceará',
+  'Distrito Federal',
+  'Espírito Santo',
+  'Goiás',
+  'Maranhão',
+  'Mato Grosso',
+  'Mato Grosso do Sul',
+  'Minas Gerais',
+  'Pará',
+  'Paraíba',
+  'Paraná',
+  'Pernambuco',
+  'Piauí',
+  'Rio de Janeiro',
+  'Rio Grande do Norte',
+  'Rio Grande do Sul',
+  'Rondônia',
+  'Roraima',
+  'Santa Catarina',
+  'São Paulo',
+  'Sergipe',
+  'Tocantins'
+];
+
+const comboEstados = document.querySelector('#comboEstados');
+
+for (let chave in estados) {
+  const option = document.createElement('option');
+  option.innerHTML = estados[chave];
+  option.value = estados[chave];
+  comboEstados.appendChild(option); 
+}
